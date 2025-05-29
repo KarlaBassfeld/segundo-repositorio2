d6772a2f9dfe45b8cff677d28972bab4f795f7e7
KarlaBassfeld <karla.bassfeld@escola.pr.gov.br>
2025-05-28 22:46:25 -0300

    28/05 2

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 Function criaCartao(categoria, pergunta, resposta) {
-console,log(categoria, peergunta, resposta)
+
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    
+    cartao.innerHTML = 
+        <div class="cartao__conteudo" >
+            <h3>${categoria} </h3>
+            <div class="cartao__conteudo__pergunta"  >
+               <p> Oque é JavaScript? </p>
+            </div>
+            <div class="cartao__conteudo__resposta"  >
+                <p> O JavaScript é uma linguagem de programação. </p>
+            </div>
+         </div>
+
+    container.appendChild(cartao)
 }

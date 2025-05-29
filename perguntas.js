d7653d66f8a549d54dd5d4bd62cf4b4d0a0bf824
KarlaBassfeld <karla.bassfeld@escola.pr.gov.br>
2025-05-28 22:16:10 -0300

    28/05/2025

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+ 'Programação',
+ 'O que é Python?',
+ 'O python é uma linguagem de programação'
+)
+criaCartao(
+ 'Geografia',
+ 'Qual é a capital da França?',
+ 'a capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+ 'Programação',
+ 'Oque é JavaScript?',
+ 'JavanScript é uma linguagem de programação'
+
+)
